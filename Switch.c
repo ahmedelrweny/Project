29078fb742c68fd6df9812367976d6b73ca62570
Ahmed Khaled <ahmedelrweny2001@hotmail.com>
2022-04-23 20:27:23 +0200

    DEN Switches

--- Switch.c
+++ Switch.c
@@ -9,17 +9,19 @@ void SW1_Init(){
 		GPIO_PORTF_DIR_R &= ~0x10;
 		GPIO_PORTF_PUR_R |= 0x10;
 		GPIO_PORTF_DATA_R &= ~0x0E;
+		GPIO_PORTF_DEN_R |=0x10;
 }
 
 void SW2_Init(){
 		GPIO_PORTF_LOCK_R = 0x4C4F434B;
-		GPIO_PORTF_CR_R |= 0x1;
-		GPIO_PORTF_AMSEL_R &= ~0x1;
+		GPIO_PORTF_CR_R |= 0x01;
+		GPIO_PORTF_AMSEL_R &= ~0x01;
 		GPIO_PORTF_PCTL_R &= ~0x0000000F;
-		GPIO_PORTF_AFSEL_R &= ~0x1;
-		GPIO_PORTF_DIR_R &= ~0x1;
-		GPIO_PORTF_PUR_R |= 0x1;
+		GPIO_PORTF_AFSEL_R &= ~0x01;
+		GPIO_PORTF_DIR_R &= ~0x01;
+		GPIO_PORTF_PUR_R |= 0x01;
 		GPIO_PORTF_DATA_R &= ~0x0E;
+		GPIO_PORTF_DEN_R |=0x01;
 }
 
 unsigned char SW1_Input(){
@@ -27,5 +29,5 @@ unsigned char SW1_Input(){
 }
 
 unsigned char SW2_Input(){
-		return GPIO_PORTF_DATA_R & 0x1;
+		return GPIO_PORTF_DATA_R & 0x01;
 }
