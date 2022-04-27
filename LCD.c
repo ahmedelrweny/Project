bae1181fb24687cbce96e91339ade9b5d07f8a8e
Stoorny <ahmedreda4000@yahoo.com>
2022-04-27 22:47:44 +0200

    Monged

--- LCD.c
+++ LCD.c
@@ -55,4 +55,22 @@ void SetCursorToRight(){
 }
 void ShiftDisplayLeft(){
 	LCD_CMD(0x07);//used to Handle Time Entry Required in Project from Right To Left
-}	
+}
+
+//LCD_Write a function that writes character on LCD
+void LCD_Write(unsigned char Data){
+	GPIO_PORTA_DATA_R = 0x04; //which means RS=1, RW=0, EN=0 to control that the entered is data not command
+	GPIO_PORTB_DATA_R = Data; //LCD has the data entered on port B 
+	GPIO_PORTA_DATA_R |= 0x01; //which means RS=1, RW=0, EN=1 to secure data entered 
+	GPIO_PORTA_DATA_R = 0x00; //which means RS=0, RW=0, EN=0 to stop changing data on LCD after writing the desired data
+	SysTick_Wait(80); // 1 us
+}
+//LCD_String function to write the whole string on LCD
+void LCD_String(unsigned char *str){   // write a string on LCD
+  int l = strlen(str);         // l = string length
+  int i=0;                    //itterator
+  while(i<l){
+		LCD_Write(str[i]);            // call the function LCD_Write 
+		i++;
+    }
+}
