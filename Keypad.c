e4f163a03af90ff3ac71d8417eadb4f9db598760
Sara Hossny <104026246+Sara-Hossny@users.noreply.github.com>
2022-04-27 23:51:58 +0200

    KayScan

--- Keypad.c
+++ Keypad.c
@@ -1,5 +1,11 @@
 #include "defines.h"
 #include <inttypes.h>
+static char symbol[4][4] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
 void keypad_Init(){
 	SYSCTL_RCGCGPIO_R |= 0x14;            //Enable clock to PORTC and PORTE  
   while ((SYSCTL_RCGCGPIO_R&0x14)==0);  //wait for clock to be setted
@@ -17,4 +23,16 @@ void keypad_Init(){
   GPIO_PORTC_DEN_R |= 0xF0;             //Set PORTC as digital pins
   GPIO_PORTE_DEN_R |= 0x1E;             //Set PORTE as digital pins
 	GPIO_PORTE_DATA_R|=0x1E;              //Intializaion of Port E
+}
+char KayScan(){
+	int i;
+	int j=0;
+	for (i=0;i<4;i++){ // set colums 
+	GPIO_PORTC_DATA_R|=0xF0;
+	GPIO_PORTC_DATA_R&=~(0x10<<i);
+	for(j=0;j<4;j++){ // set rows
+		if((GPIO_PORTC_DATA_R&0x1E)&(0x02<<j)){
+			return symbol[j][i];
+		
+	}}}
 }
