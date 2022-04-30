1e32df810918c21556d3da703414b74e48cebd46
Ahmed Khaled <ahmedelrweny2001@hotmail.com>
2022-05-01 01:29:26 +0200

    one more

--- LCD.c
+++ LCD.c
@@ -64,15 +64,15 @@ void ShiftDisplayLeft(void){
 void LCD_Write(unsigned char Data){
 	GPIO_PORTA_DATA_R = 0x10;  //which means RS=1, RW=0, EN=0 to control that the entered is data not command
 	GPIO_PORTB_DATA_R = Data;  //LCD has the data entered on port B 
-	GPIO_PORTA_DATA_R |= 0x04; //which means RS=1, RW=0, EN=1 to secure data entered 
+	GPIO_PORTA_DATA_R |= 0x04; //Enable write data
+	SysTick_Wait(80);        //waits 1 micro sec
+	GPIO_PORTA_DATA_R = 0x00; //To Disable changes for LCD
 	LCD_CMD(0x06);             //Increment from left to right
-	GPIO_PORTA_DATA_R = 0x00;  //which means RS=0, RW=0, EN=1 to stop changing data on LCD after writing the desired data
-	SysTick_Wait(80);          // 1 us
 }
 
 //LCD_String function to write the whole string on LCD
 void LCD_String(char *str){   // write a string on LCD
-  int l = strlen(str);        // l = string length
+  int l=strlen(str);        // l = string length
   int i=0;                    //itterator
   while(i<l){
 		LCD_Write(str[i]);        // call the function LCD_Write 
