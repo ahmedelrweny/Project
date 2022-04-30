84e4cad0fb0c9c321ef7cd8f5780a1071ee9ee01
Ahmed Khaled <ahmedelrweny2001@hotmail.com>
2022-04-30 20:47:21 +0200

    timer

--- Timer.c
+++ Timer.c
@@ -17,3 +17,15 @@ void Systick_Wait_ms(unsigned int time)
 		Systick_Wait_1ms();
 	}
 }
+void SysTick_Init (void) {
+	 NVIC_ST_CTRL_R = 0; 
+ 	 NVIC_ST_RELOAD_R = 0x00FFFFFF; 
+   	 NVIC_ST_CURRENT_R = 0;
+	 NVIC_ST_CTRL_R = 0x00000005; 
+}
+
+void SysTick_Wait (uint32_t delay) {
+	NVIC_ST_RELOAD_R = delay-1; 
+	NVIC_ST_CURRENT_R = 0; 
+	while ((NVIC_ST_CTRL_R&0x00010000)==0){} 
+}
