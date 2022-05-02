72d66775cafb35995dd7b81c9eb9424afcdaa416
Ahmed Khaled <ahmedelrweny2001@hotmail.com>
2022-05-02 21:36:55 +0200

    new keypad

--- Keypad.c
+++ Keypad.c
@@ -1,15 +1,12 @@
 #include "defines.h"
 #include <inttypes.h>
-
 static char symbol[4][4] = {
   {'1','2','3','A'},
   {'4','5','6','B'},
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-
-void keypad_Init(void){
-
+void keypad_Init(){
   GPIO_PORTC_CR_R |= 0xF0;              //Allow settings for all pins of PORTC
   GPIO_PORTE_CR_R  |= 0x1E;             //Allow settings for all pins of PORTE
   GPIO_PORTE_DIR_R &=~(0x1E);           //PE1-PE4 are used with row and set them as digital input pins
@@ -23,18 +20,17 @@ void keypad_Init(void){
   GPIO_PORTE_PDR_R |= 0x1E;             //Enable pull down resistor on PORTE
   GPIO_PORTC_DEN_R |= 0xF0;             //Set PORTC as digital pins
   GPIO_PORTE_DEN_R |= 0x1E;             //Set PORTE as digital pins
-	GPIO_PORTE_DATA_R&=~0x1E;             //Intializaion of PORTE
+	GPIO_PORTE_DATA_R&=~0x1E;              //Intializaion of PORTE
 }
-
-char KeyScan(void){
+char KeyScan(){
+	while(1){
 	int i;
 	int j;
-	for (i=0;i<4;i++){                    // set colums 
+	for (i=0;i<4;i++){ // set colums 
 	GPIO_PORTC_DATA_R=(0x10<<i);
-	for(j=0;j<4;j++){                     // set rows
+	for(j=0;j<4;j++){ // set rows
 		if((GPIO_PORTC_DATA_R&0x1E)&(0x02<<j)){
 			return symbol[j][i];
 		
 	}}}
-	return '\0' ;
-}
+}}
