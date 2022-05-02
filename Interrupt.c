45f266c60bff471f6c680331b782649b136a0e88
Ahmed Khaled <ahmedelrweny2001@hotmail.com>
2022-05-03 01:28:11 +0200

    interrupt v2.0

--- Interrupt.c
+++ Interrupt.c
@@ -2,9 +2,13 @@
 #include <inttypes.h>
 
 void interrupt_Init(void){
-	NVIC_EN0_R |= (1<<30); /*Enable PORTF Interrupt IRQ30 */
-	GPIO_PORTF_IM_R |= (1<<4)|(1<<0);
-	GPIO_PORTF_IS_R  &= ~(1<<4)|~(1<<0); /* make bit 4, 0 edge sensitive */
-	GPIO_PORTF_IEV_R &= ~(1<<4)|~(1<<0);  /*PF0, PF4 falling edge trigger */
+	
+	GPIO_PORTF_IS_R  &= ~(1<<4)|~(1<<0);        /* make bit 4, 0 edge sensitive */
+  GPIO_PORTF_IBE_R &=~(1<<4)|~(1<<0);         /* trigger is controlled by IEV */
+  GPIO_PORTF_IEV_R &= ~(1<<4)|~(1<<0);        /* falling edge trigger */
+  GPIO_PORTF_ICR_R |= (1<<4)|(1<<0);          /* clear any prior interrupt */
+  GPIO_PORTF_IM_R  |= (1<<4)|(1<<0);          /* unmask interrupt */
+	
+  NVIC_EN0_R |= (1<<30);  /* enable IRQ30 (D30 of ISER[0]) */
 
 }
