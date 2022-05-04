474a3d9e67280672784a360f51dcf736d64e6beb
Sara Hossny <104026246+Sara-Hossny@users.noreply.github.com>
2022-05-04 17:53:57 +0200

    Kaypad_update

--- Keypad.c
+++ Keypad.c
@@ -7,8 +7,6 @@ static char symbol[4][4] = {
   {'*','0','#','D'}
 };
 void keypad_Init(){
-	SYSCTL_RCGCGPIO_R |= 0x14;            //Enable clock to PORTC and PORTE  
-  while ((SYSCTL_RCGCGPIO_R&0x14)==0);  //wait for clock to be setted
   GPIO_PORTC_CR_R |= 0xF0;              //Allow settings for all pins of PORTC
   GPIO_PORTE_CR_R  |= 0x1E;             //Allow settings for all pins of PORTE
   GPIO_PORTE_DIR_R &=~(0x1E);           //PE1-PE4 are used with row and set them as digital input pins
@@ -17,8 +15,8 @@ void keypad_Init(){
 	GPIO_PORTE_AMSEL_R&=~(0x1E);          //Disable the analog function for PORTE
 	GPIO_PORTC_AFSEL_R&=~(0xF0);          //Disable alternate function for PORTC
 	GPIO_PORTE_AFSEL_R&=~(0x1E);          //Disable alternate function for PORTE
-	GPIO_PORTC_PCTL_R&=~(0x11110000);     //using pins as GPIO
-	GPIO_PORTE_PCTL_R&=~(0x00011110);     //using pins as GPIO
+	GPIO_PORTC_PCTL_R&=~(0xFFFF0000);     //using pins as GPIO
+	GPIO_PORTE_PCTL_R&=~(0x000FFFF0);     //using pins as GPIO
   GPIO_PORTE_PDR_R |= 0x1E;             //Enable pull down resistor on PORTE
   GPIO_PORTC_DEN_R |= 0xF0;             //Set PORTC as digital pins
   GPIO_PORTE_DEN_R |= 0x1E;             //Set PORTE as digital pins
