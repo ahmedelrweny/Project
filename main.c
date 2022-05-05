149303579d96ccd0b1e317438c90b9d90fef9f82
Stoorny <ahmedreda4000@yahoo.com>
2022-05-05 19:57:35 +0200

    Microwave.c Init //// Main editing for later on

--- main.c
+++ main.c
@@ -13,16 +13,7 @@
 char input;
 
 int main(void){
-	System_Init();
-	buzzer_Init();
-	keypad_Init();
-	LCD_Init();
-	LED_Init();
-	SW1_Init();
-	SW2_Init();
-	SW3_Init();
-	interrupt_Init();
-	
+	microwave_Init();
 	while(1){
 		/*switch(input){
 		case 1:
@@ -42,27 +33,4 @@ int main(void){
 			break;
 		}*/
 	}	
-}
-
-void GPIOF_Handler(void)
-{	
-  if (GPIO_PORTF_MIS_R & 0x10) /* check if interrupt causes by PF4/SW1*/
-    {   
-      //YOUR CODE HERE MONGED & REDA (SW1)
-      GPIO_PORTF_ICR_R |= 0x10; /* clear the interrupt flag */
-    } 
-    else if (GPIO_PORTF_MIS_R & 0x01) /* check if interrupt causes by PF0/SW2 */
-    {   
-     //YOUR CODE HERE MONGED & REDA (SW2)
-			GPIO_PORTF_ICR_R |= 0x01; /* clear the interrupt flag */
-    }
-}
-
-void GPIOD_Handler(void)
-{	
-  if (GPIO_PORTD_MIS_R & 0x04) /* check if interrupt causes by PD6/SW3*/
-    {   
-      //YOUR CODE HERE MONGED & REDA (SW3)
-      GPIO_PORTD_ICR_R |= 0x04; /* clear the interrupt flag */
-		}    
 }
