a9b636838635dda2125ebc317c349c07bdf6f971
RanaHossny <104026566+RanaHossny@users.noreply.github.com>
2022-05-05 02:29:25 +0200

    microwave.h

--- Microwave.h
+++ Microwave.h
@@ -5,5 +5,7 @@ void microwave_Init(void);
 void Cooking(char choose );
 void cook_Popcorn();
 void cook_Beef_or_Chicken(int No_kiloes , char choose);
+char* Int_to_char(int x);
+int Char_to_int(char x);
 
 #endif
