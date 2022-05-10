bf311facdb196cd20658f635fbc75e03aa9eba6c
Ahmed Khaled <ahmedelrweny2001@hotmail.com>
2022-05-10 22:47:05 +0200

    Revert "another useless space"
    This reverts commit 1b657d01d3e5201ad009305f4ddc9d090e984d27.

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ int main(void){
 	microwave_Init();
 	while(1){
 		switch(KeyScan()){
+			
 			case 'A':
 				cook_Popcorn();
 			break;
@@ -32,7 +33,6 @@ int main(void){
 			default:
 				LCD_String("Please enter a valid choice");
 			break;
-			
 		}	
 	}
 }
