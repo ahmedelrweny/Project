41c5528cd14dacf21f5c48ba2c0673fd07abbfb1
Stoorny <ahmedreda4000@yahoo.com>
2022-05-17 13:08:20 +0200

    LCD Write Improvement

--- LCD.c
+++ LCD.c
@@ -53,13 +53,13 @@ void LCD_Init(void){
 //LCD_Write a function that writes character on LCD
 void LCD_Write(unsigned char Data){
 	GPIO_PORTA_DATA_R = 0x10;  //which means RS=1, RW=0, EN=0 to control that the entered is data not command
-	Systick_Wait_1ms();
+	SysTick_Wait(8000);
 	GPIO_PORTA_DATA_R |= 0x04; //Enable write data
-	Systick_Wait_1ms();
+	SysTick_Wait(8000);
 	GPIO_PORTB_DATA_R = Data;  //LCD has the data entered on port B
-	Systick_Wait_1ms();
+	SysTick_Wait(8000);
 	GPIO_PORTA_DATA_R = 0x00; //To Disable changes for LCD
-	Systick_Wait_1ms();
+	SysTick_Wait(8000);
 	LCD_CMD(0x06);             //Increment from left to right
 }
 
