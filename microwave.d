ea1a8f3227158f5ae1b488d054782417b7a21ce9
Omar Taman <omar.a.taman01@gmail.com>
2022-05-18 15:08:29 +0200

    _Pressed

--- microwave.d
+++ microwave.d
@@ -11,4 +11,3 @@
 .\microwave.o: C:\Keil\ARM\ARMCC\bin\..\include\inttypes.h
 .\microwave.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
 .\microwave.o: Interrupt.h
-.\microwave.o: C:\Keil\ARM\ARMCC\bin\..\include\stdbool.h
